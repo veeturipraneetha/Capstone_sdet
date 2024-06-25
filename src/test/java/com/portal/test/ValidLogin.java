50f226141425ab9aa03a02e9872442a7b7b83c57
veeturipraneetha <150992908+veeturipraneetha@users.noreply.github.com>
2024-06-26 00:45:41 +0530

    Update ValidLogin.java

--- src/test/java/com/portal/test/ValidLogin.java
+++ src/test/java/com/portal/test/ValidLogin.java
@@ -13,16 +13,18 @@ public class ValidLogin extends Base {
 	public void portal_login_valid(String user,String pass) throws InterruptedException {
 		
 		Thread.sleep(2000);
-
+ // Enter valid username and valid password
 		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys(user);
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(pass);
 		Thread.sleep(1000);
-		
+		// click on submit button
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
-				
+	 // stores the title of the home page			
 	String title=driver.findElement(By.xpath("//div[@class='app_logo']")).getText();
+		// valiadte the title of the home page page after user navigate to home page
 	Assert.assertTrue(title.contains("Swag Labs"));
+		//print the title of the page
 	System.out.println(driver.getTitle());
 	
 	
