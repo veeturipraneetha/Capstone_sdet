418fa3b693395edc972af26f7e8ebcdcf708baf7
veeturipraneetha <150992908+veeturipraneetha@users.noreply.github.com>
2024-06-26 00:38:26 +0530

    Update InvalidLogin.java

--- src/test/java/com/portal/test/InvalidLogin.java
+++ src/test/java/com/portal/test/InvalidLogin.java
@@ -12,15 +12,15 @@ public class InvalidLogin extends Base {
 	public void portal_login_invalid(String invaliduser,String invalidpass) throws InterruptedException {
 		
 		Thread.sleep(2000);
-
+ // Enter invalid username and invalid password
 		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys(invaliduser);
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(invalidpass);
 		Thread.sleep(1000);
-		
+		// click on submit button
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
 		Thread.sleep(1000);		
-	
+	// validate Error Message is displayed 
 	Assert.assertTrue(driver.findElement(By.xpath("//button[@class='error-button']")).isDisplayed());
 	
 }}
